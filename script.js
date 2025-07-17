7bb9430267c098ee3e9d5e24914429c3c3afc104
haxted <vlorpprolpor@gmail.com>
2025-07-18 00:11:03 +0300

    First good commit dey

---
+++ script.js
@@ -0,0 +1,25 @@
+function register() {
+    const username = document.getElementById("usr").value.trim();
+    const password = document.getElementById("pass").value.trim();
+    if(username === "") {
+        alert("Must put a username");
+    } else if(password === "") {
+        alert("Must put a basword");
+    }
+    let userCount = parseInt(localStorage.getItem("usrcount")) || 0;
+    let currentUserId = parseInt(localStorage.getItem("currentuid")) || 0;
+    const users = JSON.parse(localStorage.getItem("usrs")) || [];
+    const newUser = {
+        username: username,
+        password: password,
+        userId: userCount + 1
+    };
+    userCount++;
+    users.push(newUser);
+    localStorage.setItem("usrs", JSON.stringify(users));
+    localStorage.setItem("usrcount", userCount);
+    localStorage.setItem("currentuid", currentUserId);
+    document.getElementById("loginreg").style.display = "none";
+    document.getElementById("scam").style.display = "block";
+    document.getElementById("congo").style.display = "block";
+}
